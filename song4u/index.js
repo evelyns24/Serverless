f34b05b70d3dc938b40036054344fc6ae0df26f8
Evelyn <evelyna.217@gmail.com>
2022-06-28 18:31:04 -0400

    complete week2 step7

--- song4u/index.js
+++ song4u/index.js
@@ -1,4 +1,5 @@
 const querystring = require("qs");
+const fetch = require("node-fetch");
 
 module.exports = async function (context, req) 
 {
@@ -7,8 +8,71 @@ module.exports = async function (context, req)
     const queryObject=querystring.parse(req.body);
     const url=queryObject.MediaUrl0;
     context.log(url);
-    
+
+    const resp = await fetch(url, {
+        method: "GET",
+    });
+
+    const data = await resp.arrayBuffer();
+
+    const result=await analyzeImage(data);
+
+    const age=result[0].faceAttributes.age;
+
+    let id ="";
+    if (age>5 && age<25)
+    {
+        id="GenZ"
+    }
+    else if (age>24 && age<41)
+    {
+        id="GenY"
+    }
+    else if (age>40 && age<57)
+    {
+        id="GenX"
+    }
+    else if (age>56 && age<76)
+    {
+        id="BabyBoomers"
+    }
+    else
+    {
+        id="Unknown"
+    }
+
+    context.log(id);
+
     context.res = {
-        body: url,
+        body: id,
     };
 };
+
+
+async function analyzeImage(img)
+{
+    const subKey = process.env.SUBSCRIPTIONKEY;
+    const uriBase = process.env.ENDPOINT + "/face/v1.0/detect";
+   
+
+    const params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'age',
+    });
+
+    // making the post request
+    const resp = await fetch(uriBase + '?' + params.toString(),{
+        method: 'POST',
+        body: img,
+        // img is the parameter inputted
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': subKey
+        }
+    });
+
+    // receive the response
+    const data = await resp.json();
+
+    return data;
+}
